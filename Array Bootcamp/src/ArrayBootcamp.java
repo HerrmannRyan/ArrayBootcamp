38f19b1c13779be2c8ce6d4fd1e1135ef5f747cc
ryan.herrmann <ryan.herrmann@mu-c1-17.mullen.pvt.k12.co.us>
2019-09-05 14:10:35 -0600

    Finished and working

--- Array Bootcamp/src/ArrayBootcamp.java
+++ Array Bootcamp/src/ArrayBootcamp.java
@@ -4,11 +4,11 @@ public class ArrayBootcamp
 	{
 
 		static Scanner userInput = new Scanner(System.in);
-		
+
 		public static void main(String[] args)
 			{
-				//displayUsersFavoriteCandy();
-				//totalUpEvenAndOddNumbers();
+				displayUsersFavoriteCandy();
+				totalUpEvenAndOddNumbers();
 				generateAndPrintFiveRandomNumbers();
 
 			}
@@ -17,69 +17,81 @@ public class ArrayBootcamp
 			{
 				System.out.println("How many favorite candies do you have?");
 				int numberOfFavoriteCandies = userInput.nextInt() + 1;
-				System.out.println("Sweet! You have " + (numberOfFavoriteCandies - 1) + " favorite candies. Please name them down below on separate lines.");	
-				
-				String[] usersFavoriteCandies = new String[numberOfFavoriteCandies] ;
-				
+				System.out.println("Sweet! You have " + (numberOfFavoriteCandies - 1)
+						+ " favorite candies. Please name them down below on separate lines.");
+
+				String[] usersFavoriteCandies = new String[numberOfFavoriteCandies];
+
 				for (int i = 0; i < numberOfFavoriteCandies; i++)
-				{
-					
-					usersFavoriteCandies [i] = userInput.nextLine();
-					
-				}
-				
+					{
+
+						usersFavoriteCandies[i] = userInput.nextLine();
+
+					}
+
 				System.out.println("Okay! Your favorite candies are:");
-				
+
 				for (String s : usersFavoriteCandies)
-				{
-					System.out.println(s);	
-				}
-				
+					{
+						System.out.println(s);
+					}
+
 			}
 
 		public static void totalUpEvenAndOddNumbers()
 			{
-				
-				System.out.println("Hello Please insert 8 numbers for me to add the even and odd numbers.");
-				
-				int[] usersEightNumbers = new int [8];
-				
-				for (int i = 0;  i < 8; i++)
-				{
-					usersEightNumbers [i] = userInput.nextInt();	
-				}
-				
+
+				System.out.println("Please insert 8 numbers for me to add the even and odd numbers.");
+
+				int[] usersEightNumbers = new int[8];
+
+				for (int i = 0; i < 8; i++)
+					{
+						usersEightNumbers[i] = userInput.nextInt();
+					}
+
 				int sumOfOddNumbers = 0;
 				int sumOfEvenNumbers = 0;
-				
+
 				for (int s : usersEightNumbers)
-				{
-					if (s % 2 == 0)
 					{
-						sumOfEvenNumbers += s;	
+						if (s % 2 == 0)
+						{
+							sumOfEvenNumbers += s;
+						} 
+						else
+						{
+							sumOfOddNumbers += s;
+						}
+
 					}
-					else
-					{
-						sumOfOddNumbers += s;
-					}
-						
-				}
-				
-				System.out.println("The sum of the even numbers is " + sumOfEvenNumbers + ", and the sum of the odd numbers is " + sumOfOddNumbers);
-				
+
+				System.out.println("The sum of the even numbers is " + sumOfEvenNumbers
+						+ ", and the sum of the odd numbers is " + sumOfOddNumbers);
+
 			}
 
 		public static void generateAndPrintFiveRandomNumbers()
 			{
 				System.out.println("The five random two digit numbers are:");
-				
+
 				int[] randomTwoDigitNumbers = new int[5];
 				for (int i = 0; i < 5; i++)
-				{
-					randomTwoDigitNumbers [i] = (int)(Math.random()*90 + 10);
-					
-				}
+					{
+						randomTwoDigitNumbers[i] = (int) (Math.random() * 90 + 10);
+					}
+
+				for (int s : randomTwoDigitNumbers)
+					{
+						System.out.println(s);
+					}
 				
+				System.out.println("Backwards they are:");
+				for (int i = 4; i >= 0; i--)
+					{
+						System.out.println(randomTwoDigitNumbers[i]);
+					}
+
 			}
 
 	}
